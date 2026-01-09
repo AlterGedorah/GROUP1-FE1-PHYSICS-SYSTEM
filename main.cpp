762baf6a26bc90e78de4f237b533349722a9180e
Emdee-git <ransclemenemdeevringor@iskolarngbayan.pup.edu.ph>
2026-01-10 03:49:41 +0800

    not final, finding oppurtunities

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ void showMenu() {
     cout << "Physics System\n";
     cout << "0 - Exit\n";
     cout << "1 - Velocity\n";
+    cout << "2 - Acceleration\n";
     cout << "7 - Energy/Work\n";
     cout << "8 - Power\n";
     cout << "Enter your choice: ";
@@ -20,12 +21,21 @@ public:
     double calculateWork(double force, double distance) {
         return force * distance;
     }
+
+    double calculateVelocity(double distance, double time) {
+        return distance / time;
+    }
+
+    double calculateAcceleration(double velocity, double time, double initialVelocity) {
+        return (velocity - initialVelocity) /  time;
+    }
 };
+
 
 int main() {
     PhysicsSystem physicsCalculator;
     int choice;
-    double force, velocity, distance;
+    double force, velocity, distance, time, initialVelocity;
 
     while (true) {
         showMenu();
@@ -42,6 +52,37 @@ int main() {
             case 0:
                 cout << "Exiting program.\n";
                 return 0;
+
+            case 1: 
+                cout << "Calculate Velocity\n";
+                cout << "Enter the Distance (D):";
+                cin >> distance;
+                cout << "Enter the Time (T):";
+                cin >> time;
+            if (time == 0 ){
+                cout << "Invalid Value";
+            } else{cout << "Velocity ="
+                    << physicsCalculator.calculateVelocity(distance, time)
+                    << "m/s\n";}
+
+                    break;
+
+            
+            case 2: 
+                cout << "Calculate Acceleration\n";
+                cout << "Enter the Velocity (V):";
+                cin >> velocity;
+                cout << "Enter The Initial Velocity (Vu):";
+                cin >> initialVelocity;
+                cout << "Enter the Time (T):";
+                cin >> time;
+
+                cout << "Acceleration = "
+                    << physicsCalculator.calculateAcceleration(velocity, time, initialVelocity)
+                    << "m/s²\n";
+
+                break;
+
 
             case 7:
                 cout << "Calculate Energy/Work\n";
