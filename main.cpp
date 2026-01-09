9ca95be16e280d203c5abb1da2c22b74c1f6ceec
Paul Joshua Dacoliat <dacoliatpaul@gmail.com>
2026-01-09 21:38:54 +0800

    ADDED WORK AND POWER 7, 8

--- main.cpp
+++ main.cpp
@@ -6,45 +6,53 @@ void showMenu(){
     cout << "0 - Exit\n";
     cout << "7 - Energy/Work\n";
     cout << "8 - Power\n";
-    cout << "Enter choice: ";
+    cout << "Enter your choice: ";
 }
 class PhysicsSystem {
     public:
     double measurement;
-   
+
+    double calculatePower(double force, double velocity) {
+        return force * velocity;
+        cout << "Power = " << calculatePower(force, velocity) << " Watts"<< endl;
+    }
+    double calculateWork(double force, double distance) {
+        return force * distance;
+        cout << "Energy = " << calculateWork(force, distance) << " Joules"<< endl;
+    }
     
 };
 
 int main(){
+    PhysicsSystem physicsCalculator;
     int choice;
     double force, velocity, distance;
+    
     while (true) {
         showMenu();
         cin >> choice;
         if (choice == 0) {
             break;
         }
-    
-    if (choice == 7){
+
+        cout << "input Variables:\n";
+        
+
+        if (choice == 7){
             cout << "Calculate Energy/Work\n";
-            cout << "Enter the force (N): ";
+            cout << "Enter the force (N): "; 
             cin >> force;
             cout << "Enter the distance (m): ";
-            cin >> distance;
-            cout << "Energy = " << force * distance << " Joules"<< endl;
-
-
+            cin >> distance; 
+            cout << "Energy = " << physicsCalculator.calculateWork(force, distance) << " Joules"<< endl;
         } else if (choice == 8){
-                cout << "Calculate Power\n";
+            cout << "Calculate Power\n";
             cout << "Enter the force (N): ";
             cin >> force;
             cout << "Enter the velocity (m/s): ";
-            cin >> velocity;
-            cout << "Power = " << force * velocity << " Watts"<< endl;
-
-        } else {
-            cout << "Invalid Choice!, Please try again\n"; 
+            cin >> velocity; 
+            cout << "Power = " << physicsCalculator.calculatePower(force, velocity) << " Watts"<< endl;
         }
     }
     return 0;
-}   
+}
